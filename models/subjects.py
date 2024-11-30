6536dd5eaedcf5e21e1bc06389adad218ecf42ba
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-30 15:38:38 +0300

    modifications

--- models/subjects.py
+++ models/subjects.py
@@ -1,7 +1,11 @@
-class Subject:
-    def __init__(self, name, code):
-        self.name = name
-        self.code = code
+# class Subject:
+#     def __init__(self, name, code):
+#         self.name = name
+#         self.code = code
 
-    def __repr__(self):
-        return f"Subject(name={self.name}, code={self.code})"
+#     def __repr__(self):
+#         return f"Subject(name={self.name}, code={self.code})"
+class Subject:
+    def __init__(self, name, duration):
+        self.name = name
+        self.duration = duration

33a23437fc1ab7d1be3a947e5bad29bbf1830c86
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-30 15:38:13 +0300

    modifications

--- models/trainers.py
+++ models/trainers.py
@@ -1,7 +1,4 @@
 class Trainer:
-    def __init__(self, name, trainer_id):
+    def __init__(self, name, subject):
         self.name = name
-        self.trainer_id = trainer_id
-
-    def __repr__(self):
-        return f"Trainer(name={self.name}, trainer_id={self.trainer_id})"
+        self.subject = subject

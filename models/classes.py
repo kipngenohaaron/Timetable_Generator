21a66fe7587559f6921ac91352bc12dfd3fb95a9
kipngenohaaron <kipngenohaaron@gmail.com>
2024-11-30 15:37:49 +0300

    modifications

--- models/classes.py
+++ models/classes.py
@@ -1,9 +1,16 @@
-class Class:
-    def __init__(self, name, code, duration=None, venue=None):
-        self.name = name
-        self.code = code
-        self.duration = duration  # Duration of the class
-        self.venue = venue        # Venue of the class
+# class Class:
+#     def __init__(self, name, code, duration=None, venue=None):
+#         self.name = name
+#         self.code = code
+#         self.duration = duration  # Duration of the class
+#         self.venue = venue        # Venue of the class
 
-    def __repr__(self):
-        return f"Class(name={self.name}, code={self.code}, duration={self.duration}, venue={self.venue})"
+#     def __repr__(self):
+#         return f"Class(name={self.name}, code={self.code}, duration={self.duration}, venue={self.venue})"
+class Class:
+    def __init__(self, name, subjects, trainer, venue, duration):
+        self.name = name
+        self.subjects = subjects
+        self.trainer = trainer
+        self.venue = venue
+        self.duration = duration
